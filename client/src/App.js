d9c69751fa7cbb1699329cc33a87596420fac150
OsbaldoPalacios <osbaldo.palacios@uaem.edu.mx>
2023-06-15 16:35:37 -0600

    create client & server

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="datos">
+        <label>Nombre: <input type="text" /></label><br/>
+        <label>Edad: <input type="text" /></label><br/>
+        <label>País: <input type="text" /></label><br/>
+        <label>Cargo: <input type="text" /></label><br/>
+        <label>Años: <input type="text" /></label><br/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
